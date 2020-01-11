1efa3975e97009dce9901a4e3b717621ce605012
konrad ullrich <konradullrich@me.com>
2020-01-11 16:19:10 +0100

    not all blocks

--- features/src/blocks.js
+++ features/src/blocks.js
@@ -10,17 +10,17 @@
  */
 // import "./show-posts/show-posts.js";
 // import "./hero2/hero.js";
-import "./hero/hero.js";
+// import "./hero/hero.js";
 // import "./date/date.js";
 // import "./fadein/fadein.js";
 // import "./sozial_links/sozial_links";
 // import "./featuredImageFocal/featuredImageFocal.js";
 // import "./gallery/gallery";
-import "./amhHeader/amhHeader";
-import "./projectsGallerie/projectsGallerie";
+// import "./amhHeader/amhHeader";
+// import "./projectsGallerie/projectsGallerie";
 import "./readmore/readmore-blocks";
 // import "./mailchimp/mailchimp-blocks";
-import "./events/events-blocks";
-import "./gridGallerie/gridGallerie";
-import "./icons/icons-blocks";
+// import "./events/events-blocks";
+// import "./gridGallerie/gridGallerie";
+// import "./icons/icons-blocks";
 import "./AmhReadmore/readmore-blocks";
